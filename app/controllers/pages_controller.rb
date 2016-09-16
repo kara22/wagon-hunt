b616c090d5f1d4f4e608163427437a7ac3e4f80f
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-16 21:25:29 +0200

    add model Product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
   end
+
+  def team
+    @members = ["kara", "imane", "adam"]
+  end
+
+  def join_us
+  end
+
 end
