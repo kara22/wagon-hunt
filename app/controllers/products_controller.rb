757ff0b7c40f1f5b5b3ea0a139f0387642a6aaf9
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-17 20:46:41 +0200

    refacto front end

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :url)
+    params.require(:product).permit(:name, :url, :tagline)
   end
 
   def find_product
