a8ba79cfa11f68e3a0ab99bb4b6ccfaf0c5ac293
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-18 11:50:19 +0200

    add category to products + implement search by category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916192721) do
+ActiveRecord::Schema.define(version: 20160918092045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160916192721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "tagline"
+    t.string   "category"
   end
 
 end
